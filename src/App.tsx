41241bb4d30ddeb9811aadf319224a8a1d8dc58e
Murat <144884820+Mikarmk@users.noreply.github.com>
2025-02-20 10:44:38 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router';
 
 function App() {
-    return <AppRouter />;
+    return (
+        <BrowserRouter basename="/mikarflex">
+            <AppRouter />
+        </BrowserRouter>
+    );
 }
 
 export default App;
